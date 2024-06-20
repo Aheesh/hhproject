69b54a866f2f9c1dd3792c33761b37228396f64f
aheesh <aheesh@gmail.com>
2024-06-19 20:21:26 -0700

    refactor: Update PlayerBToken deployment arguments and set controller address

--- chain/contracts/PlayerBToken.sol
+++ chain/contracts/PlayerBToken.sol
@@ -8,21 +8,29 @@ pragma solidity >=0.7.0 <0.9.0;
  */
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+import "@openzeppelin/contracts/access/Ownable.sol";
 import "hardhat/console.sol";
 
-contract PlayerBToken is ERC20 {
+contract PlayerBToken is ERC20, Ownable {
+    address private deployer;
+    address private vault;
+    address private controller;
+
     event PlayerBTokenApproved(
         address indexed owner,
         address indexed spender,
         uint256 value
     );
 
-    constructor(uint256 amount) ERC20("PlayerB", "PB") {
+    constructor(address _vault, uint256 amount) ERC20("PlayerB", "PB") {
         console.log(
             unicode"💸💸💸MINT-Player B💸💸💸",
             "constructor : MarketToken B with initial supply of",
             amount
         );
+        deployer = msg.sender;
+        vault = _vault;
+        console.log("Contructor() - Token B deployer address", deployer);
         _mint(msg.sender, amount * 10 ** 18);
     }
 
@@ -35,7 +43,40 @@ contract PlayerBToken is ERC20 {
         return super.approve(spender, amount);
     }
 
-    // function checkBalance(address account) public view returns (uint256) {
-    //     return balanceOf(account);
-    // }
+    function transferManager(address _from) private view returns (bool) {
+        if (_from == deployer || _from == vault || _from == controller)
+            return true;
+        else return false;
+    }
+
+    function transfer(
+        address _to,
+        uint256 _value
+    ) public override returns (bool) {
+        require(
+            transferManager(msg.sender),
+            "Transfer permitted only from token deployer, controller or vault"
+        );
+        console.log(" transfer call from msg.sender ==>", msg.sender);
+        super.transfer(_to, _value);
+        return true;
+    }
+
+    function transferFrom(
+        address _from,
+        address _to,
+        uint256 _value
+    ) public override returns (bool) {
+        require(
+            transferManager(_from),
+            "Transfer only permitted from deployer or vault"
+        );
+        super.transferFrom(_from, _to, _value);
+        return true;
+    }
+
+    function setController(address _controller) public onlyOwner {
+        controller = _controller;
+        console.log("Controller set to", _controller);
+    }
 }
